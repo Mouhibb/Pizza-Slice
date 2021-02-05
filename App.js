f8b9a5ed5739722d96f15f85c525a6f001793baa
Mouhibb <45210150+Mouhibb@users.noreply.github.com>
2021-02-05 02:47:53 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import { Hero } from "./components/Hero";
+import { BrowserRouter as Router } from 'react-router-dom';
+import {GlobalStyle} from './globalStyles';
+
+
+function App() {
+  return (
+
+
+   
+
+     <Router>
+       <GlobalStyle />
+       <Hero />
+     
+     
+     
+     
+     
+     </Router>
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+  );
+}
+
+export default App;
